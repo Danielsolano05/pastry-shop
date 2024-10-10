0e83e375046bac38705799254a1da7ab6d15d8f2
daniel solano <Danielsolano05@gmail.com>
2024-10-10 15:03:17 -0700

    update

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let userName = prompt('What is your name?');
+console.log('Hello', + userName); 
+
+
+let pastry = prompt('on a scale of how much do you like pastrys (1-10)');
+let message;
+
+if (pastry >= 7){
+    message = 'love pastrys';
+} else if (pastry >=5 ){
+    message = 'there ok';
+} else if (pastry >=1){
+    message = 'hate them';
+} else {
+    message = 'pastry likeness does not exist';
+}
+
+let sleep = 6; 
+if (sleep <=6) {
+    console.log('you need more sleep' +);
+}
+
+
+document.write('Hello ' + userName + '! ' + message);
+
+
